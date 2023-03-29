085c9708457d94ede66782a917c968c17a55f227
Кунах Айдунбеков <Karaxari>
2023-03-29 23:11:14 +0300

    first commit 2

---
+++ ConsoleMessenger/DataController.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "MessangerUser.h"
+
+const string pathUsersData = "DataController\\allUsers.txt"; // данные пользователей: Имя, логин и пароль
+const string pathGeneralChatData = "DataController\\generalChat.txt"; // переписка общего чата
+
+class DataController
+{
+public:
+	DataController();
+	~DataController() = default;
+
+	void dataLoader();
+	void setUser(MessangerUser user);
+	void setMessageGeneralChat(string message);
+	void loadUser(MessangerUser user);
+
+	void outUsers();
+	void outMessagesGeneralChat();
+
+	bool authentication(string login, string password);
+	bool loginVerification(string login);
+	void authorization(string login, string password);
+
+private:
+	map<string, MessangerUser> _listOfUsers; // Закрытое наслеование, релизация отнощения "содержит"
+	list <string> _generalСhat;
+
+	void personalAccount(string login);
+	void generalChat(string login);
+	void privateChat(string login);
+	void createNewPrivateChat(string login);
+	void sendingMessagesGeneralChat(string login);
+};
